d04287763734ea9850fc0022ae3074a8759eef36
williamsduarte <williamduarteoficial@gmail.com>
2017-06-21 20:25:05 -0300

    Adicionado typehinting em CredentialsTenant

--- src/loojas/helpers/CredentialsTenant.php
+++ src/loojas/helpers/CredentialsTenant.php
@@ -2,6 +2,8 @@
 
 namespace Loojas\Helpers;
 
+use LogicException;
+
 /**
  * Class CredentialsTenant
  * @package Loojas\Helpers
@@ -10,44 +12,52 @@ class CredentialsTenant
 {
 
     /**
-     * @var GuidProvider
-     */
-    private $guid;
-
-    /**
-     * CredentialsTenant constructor.
-     */
-    public function __construct()
-    {    
-        $this->guid = new GuidProvider();
-    }
-   
-    /**
      * This function will return a Random Database
      *
+     * @param int $id_tenant
      * @return string
      */
-    public function getUser()
+    public function getUser(int $id_tenant)
     {
-        return sprintf('_%s_user', $this->random() );
+        $guid = new GuidProvider();
+        return sprintf('_%d%s_user',$id_tenant, $this->randomUser($guid) );
     }
 
     /**
      * This function will return a Random User Database
      *
+     * @param int $id_tenant
      * @return string
      */
-    public function getDatabase()
+    public function getDatabase(int $id_tenant)
     {
-        return sprintf('_%s', $this->random() );
+        $guid = new GuidProvider();
+        return sprintf('_%d%s', $id_tenant, $this->randomDatabase($guid) );
+    }
+
+    private function separate($guid)
+    {
+        return explode('-', $guid );
     }
 
     /**
+     * @param string $guid
      * @return mixed|string
      */
-    private function random()
-    {    
-        return mb_strtolower( end( explode('-', $this->guid->getGUID()) ), 'UTF-8' );
+    private function randomDatabase($guid='')
+    {
+        $exp = $this->separate($guid);
+        return mb_strtolower( current( $exp ), 'UTF-8');
+    }
+
+    /**
+     * @param string $guid
+     * @return mixed|string
+     */
+    private function randomUser($guid='')
+    {
+        $exp = $this->separate($guid);
+        return mb_strtolower( next( $exp ), 'UTF-8');
     }
 
 }
